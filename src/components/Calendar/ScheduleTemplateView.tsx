10bda9ffa53f1987e217344de7da4a792dc5bc0e
Jarmo Penttilä <jopent@gmail.com>
2025-07-12 22:06:56 +0300

    Update ScheduleTemplateView.tsx

--- src/components/Calendar/ScheduleTemplateView.tsx
+++ src/components/Calendar/ScheduleTemplateView.tsx
@@ -57,7 +57,7 @@ export default function ScheduleTemplateView() {
           className="flex items-center px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors"
         >
           <Plus className="w-4 h-4 mr-2" />
-          Lisää tuntiryhmä
+          Lisää tuntiryhmään
         </button>
       </div>
 
