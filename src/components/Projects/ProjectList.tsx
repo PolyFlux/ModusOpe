9dff78612171cf201c0e0ce787383ccac1826f1f
Jarmo Penttilä <jopent@gmail.com>
2025-07-13 21:53:53 +0300

    Update ProjectList.tsx

--- src/components/Projects/ProjectList.tsx
+++ src/components/Projects/ProjectList.tsx
@@ -21,7 +21,7 @@ export default function ProjectList() {
   };
 
   return (
-    <div className="space-y-6">
+    <div className="p-4 md:p-8">
       {/* Header */}
       <div className="flex items-center justify-between">
         <div>
