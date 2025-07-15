dca7de2ea808ca8bdaaa7118b4d4f5c95dca2e02
Jarmo Penttilä <jopent@gmail.com>
2025-07-15 22:03:02 +0300

    Update WeekView.tsx

--- src/components/Calendar/WeekView.tsx
+++ src/components/Calendar/WeekView.tsx
@@ -18,7 +18,7 @@ export default function WeekView() {
       if (scrollContainerRef.current) {
         scrollContainerRef.current.scrollTop = 7 * 48;
       }
-    }, 500);
+    }, 50);
 
     return () => clearTimeout(timer);
   }, [state.selectedDate, state.currentView, showWeekend]);
